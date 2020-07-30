45c45b4f498e1e2e8c6cf3f4d5017c202aa76d57
Harshvardhan <harshvardhanvipat@gmail.com>
2020-07-30 17:00:45 +1000

    exported the component and added to the app.js

---
+++ src/homepage.component.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+// this is a functional component as we don't need any lifecycle  methods as of now and also we donot need any states we want to store or change
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">Shop now</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">CAPS</h1>
+          <span className="subtitle">Shop now</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">SNEAKERS </h1>
+          <span className="subtitle">Shop now</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">WOMEN </h1>
+          <span className="subtitle">Shop now</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MEN </h1>
+          <span className="subtitle">Shop now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
