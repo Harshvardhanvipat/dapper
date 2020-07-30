cd4796118f5dcd6dbf1f519d6777e1adb736e6c0
Harshvardhan <harshvardhanvipat@gmail.com>
2020-07-30 17:32:48 +1000

    added sass using yarn add

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./homepage.styles.scss";
+
 // this is a functional component as we don't need any lifecycle  methods as of now and also we donot need any states we want to store or change
 const HomePage = () => (
   <div className="homepage">
