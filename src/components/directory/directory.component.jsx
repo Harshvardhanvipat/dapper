00c0d4dfc7c021fedb00f354f05a32b455819eca
Harshvardhan <harshvardhanvipat@gmail.com>
2020-08-04 16:15:45 +1000

    added content to the home page also added styling to the pages

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./directory.style.scss";
+import MenuItem from "../menu-item/menu-item.component";
+
+// Directory component is going to be a class component
+// reason being we need to store the state and pass the details to the menu items
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    // in the below state we are storing the states of the menu-items
+    // basically it holds a link to the image image, id, title, and an urllink
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          id: 1,
+          linkUrl: "shop/hats"
+        },
+        {
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          id: 2,
+          linkUrl: "shop/jackets"
+        },
+        {
+          title: "sneakers",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          id: 3,
+          linkUrl: "shop/sneakers"
+        },
+        {
+          title: "womens",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          size: "large",
+          id: 4,
+          linkUrl: "shop/womens"
+        },
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          size: "large",
+          id: 5,
+          linkUrl: "shop/mens"
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,46 +1,12 @@
 import React from "react";
 
+import Directory from "../../components/directory/directory.component";
 import "./homepage.styles.scss";
 
 // this is a functional component as we don't need any lifecycle  methods as of now and also we donot need any states we want to store or change
 const HomePage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subtitle">Shop now</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">CAPS</h1>
-          <span className="subtitle">Shop now</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">SNEAKERS </h1>
-          <span className="subtitle">Shop now</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">WOMEN </h1>
-          <span className="subtitle">Shop now</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MEN </h1>
-          <span className="subtitle">Shop now</span>
-        </div>
-      </div>
-    </div>
+    <Directory />
   </div>
 );
 
